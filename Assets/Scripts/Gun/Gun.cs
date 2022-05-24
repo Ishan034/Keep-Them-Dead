045cb00757d94d9f108f22fdbf1481ab227392b6
Ishan034 <76897280+Ishan034@users.noreply.github.com>
2022-05-24 11:58:42 +0530

    completed the gun system

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,30 +1,94 @@
 using UnityEngine;
+using System.Collections;
+using TMPro;
 
 public class Gun : MonoBehaviour
 {
+    private float nextTImeToFire = 0f;
+    private int currentAmmo;
+    private bool isReloading = false;   
+
     [SerializeField] private float damage;
     [SerializeField] private float range;
+    [SerializeField] private float impactForce;
+    [SerializeField] private float fireRate;
+
+    [SerializeField] private int maxAmmo;
+    [SerializeField] private float reloadTime;
+    [SerializeField] private TMP_Text ammoCounter;
+    [SerializeField] private Animator animator;
 
     [SerializeField] private Camera cam;
+    [SerializeField] private ParticleSystem muzzelFlash;
+    [SerializeField] private GameObject impactEffect;
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
         {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTImeToFire)
+        {
+            nextTImeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
+
+        ammoCounter.text = currentAmmo.ToString();
+    }
+
+    private IEnumerator Reload()
+    {
+        isReloading = true;
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - 0.25f);
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(0.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
 
     private void Shoot()
     {
+        muzzelFlash.Play();
+
+        currentAmmo--;
+
         RaycastHit hit;
+
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
         {
-            EnemyTarget enemyTarget = hit.transform.GetComponent<EnemyTarget>();
-            if (enemyTarget != null)
+            EnemyTarget target = hit.transform.GetComponent<EnemyTarget>();
+            if (target != null)
             {
-                enemyTarget.TakeDamage(damage);
+               target.TakeDamage(damage);  
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactObject = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactObject, 2f);
         }
     }
 }
---
+++ Assets/Scripts/Gun/WeaponSwitcher.cs
@@ -0,0 +1,73 @@
+using System;
+
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    [SerializeField] private int selectedWeapon = 0;
+
+    private void Start()
+    {
+        SelectWeapon();
+    }
+
+    private void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedWeapon >= transform.childCount - 1)
+                selectedWeapon = 0;
+            else
+                selectedWeapon++;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedWeapon <= 0)
+                selectedWeapon = transform.childCount - 1;
+            else
+                selectedWeapon--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectedWeapon = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            selectedWeapon = 3;
+        }
+
+        if (previousSelectedWeapon != selectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+    private void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == selectedWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+
+            i++;
+        }
+    }
+}
